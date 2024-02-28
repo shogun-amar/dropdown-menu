0b294305ace1b4785b87fa2122cdeabd3f26e6f5
shogun-amar <128471454+shogun-amar@users.noreply.github.com>
2024-02-29 03:19:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACgW4m0P3HQmDCFQIWWxrpUNRn7JBgS3o",
+  authDomain: "dropdown-menu-6d2dc.firebaseapp.com",
+  projectId: "dropdown-menu-6d2dc",
+  storageBucket: "dropdown-menu-6d2dc.appspot.com",
+  messagingSenderId: "694707405682",
+  appId: "1:694707405682:web:cfe90c53f2f05a7ce253eb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +28,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
