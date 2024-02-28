8c93264ec2e0dfbef3dfc6bc486491c7827432ad
shogun-amar <128471454+shogun-amar@users.noreply.github.com>
2024-02-29 03:18:22 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  // for handling onMouseEnter
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  }
+
+  // for handling onMouseLeave
+  const handleMouseLeave = () => { 
+    setIsHovered(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dropdown">
+      <h1> Should you use a Dropdown? </h1>
+      <button onMouseEnter={handleMouseEnter} className='dropdown-btn' > Select &#9660; </button> {/* &#9660 is unique code for dropdown arrow, got it from google */}
+      {/* if isHovered is true, it will show dropdown menu item */}
+      {isHovered && (
+        <div onClick={handleMouseLeave}>
+          <p> Yes </p>
+          <p> No </p>
+          <p> Maybe </p>
+        </div>  
+      )}
     </div>
   );
 }
